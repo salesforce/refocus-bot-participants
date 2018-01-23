0024cfe5094fa6b8f162b7ecb3de27511a2559d9
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2018-01-23 12:26:20 -0500

    Get first load working

--- web/components/App.jsx
+++ web/components/App.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 const React=require('react');
 const env = process.env.NODE_ENV || 'dev';
+const config = require('../../config.js')[env];
+const bdk = require('@salesforce/refocus-bdk')(config);
 
 class App extends React.Component{
   constructor(props){
@@ -8,15 +12,44 @@ class App extends React.Component{
     this.state= {
       roomId: this.props.roomId,
       users: this.props.users,
+      value: [],
     };
+
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.toggleRtl = this.toggleRtl.bind(this);
   }
 
   // componentWillReceiveProps(nextProps) {
   // }
 
+  handleSelectChange (value) {
+    const values = value.split(',');
+    if (Array.isArray(values)) {
+      this.setState({ value: values });
+    } else {
+      this.setState({ value: [values] });
+    }
+  }
+
+  toggleRtl (e) {
+    const rtl = e.target.checked;
+    this.setState({ rtl });
+  }
+
   render() {
+    const options = ['test', 'test2'];
+
     return (
       <div>
+        <Select
+          multi
+          onChange={this.handleSelectChange}
+          options={options}
+          placeholder="Choose Incident Commander"
+          rtl={this.state.rtl}
+          simpleValue
+          value={this.state.value}
+        />
       </div>
     );
   }
