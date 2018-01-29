6ed2827ee6cbc3c3c9d7a5d072a11d889787fb19
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2018-01-29 13:37:00 -0500

    remove variables

--- config.js
+++ config.js
@@ -6,29 +6,21 @@
 module.exports = {
   dev: {
     refocusUrl: 'http://localhost:3000',
-    host: 'localhost',
-    port: '3000',
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   staging: {
     refocusUrl: 'http://refocus-staging.herokuapp.com',
-    host: 'refocus-staging.herokuapp.com',
-    port: '',
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   sandbox: {
     refocusUrl: 'https://refocus-sandbox.hk.salesforce.com',
-    host: 'refocus-sandbox.hk.salesforce.com',
-    port: '',
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   production: {
     refocusUrl: 'https://refocus.hk.salesforce.com',
-    host: 'refocus.hk.salesforce.com',
-    port: '',
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
